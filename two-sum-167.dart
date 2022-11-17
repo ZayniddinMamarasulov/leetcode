2b69af912a4d6bcfa18425afe4964eb28a1b0d9d
Zayniddin <tuit0778@gmail.com>
2022-11-17 19:52:24 +0500

    first

---
+++ two-sum-167.dart
@@ -0,0 +1,19 @@
+void main() {
+  print(twoSum([2, 7, 11, 15], 9));
+}
+
+List<int> twoSum(List<int> nums, int target) {
+  List<int> res = [];
+
+  for (int i = 0; i < nums.length; i++) {
+    for (int k = i; k < nums.length; k++) {
+      if (nums[i] + nums[k] == target) {
+        if (i < k) {
+          res.add(i + 1);
+          res.add(k + 1);
+        }
+      }
+    }
+  }
+  return res;
+}
