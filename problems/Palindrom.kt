13b7377cf51d0f0c0b68d13ac3c9f9504e3c398a
Zayniddin <tuit0778@gmail.com>
2023-06-13 23:02:04 +0500

    palindrom

---
+++ problems/Palindrom.kt
@@ -0,0 +1,13 @@
+package day20
+
+
+fun main(args: Array<String>) {
+    println(isPalindrome(10))
+}
+
+fun isPalindrome(x: Int): Boolean {
+    for ((index) in x.toString().withIndex()) {
+        if (x.toString()[index] != x.toString()[x.toString().length - index - 1]) return false
+    }
+    return true
+}
